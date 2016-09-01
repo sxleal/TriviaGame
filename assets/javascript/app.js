9210a65a96f4e05bdcc7b0c349d38b936c9ff142
Sandra Leal <sxleal@Sandras-MacBook-Air.local>
2016-09-01 13:13:10 -0500

    Stumped on jquery

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+
+	var questions = [
+		{question: "What is Romeo's last name?",
+		choices: ["Demiterious","Montague","Camelot"],
+		correctAnswer: 1
+	},{
+		question: "What is the capital of Texas?",
+		choices: ["Dallas","Houston","Austin"],
+		correctAnswer: 2
+	}, {
+		question: "What year did Columbus discover America?",
+		choices: ["1492", "1503","1962"],
+		correctAnswer: 0
+	}]
+
+	var currentQuestion = "dummy";
+
+//confirm connection between html and js files
+console.log("We see each other!");
+
+//Load a question into the html <p> with class 'questions'
+
+
+
+
+
+//Load the answer choices into html with div class=answerSpot
+
+
+
+//Check if answer clicked is same as correct,  Check if this is the last question.  If true, empty
+//the answerSpot and alert that game is done
+
+
+//addChecker function adds div to page with correct or incorrect
+
+//Start the quiz right away
+
+
+
